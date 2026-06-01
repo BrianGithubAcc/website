074f994eb9d8d6f850a574cf88110afa8004468a
BrianGitHubAcc <113306620+BrianGithubAcc@users.noreply.github.com>
2026-06-01 14:16:45 +0100

    Rename doc to docs

--- src/main.py
+++ src/main.py
@@ -41,9 +41,9 @@ def copy_static(src, dst):
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    copy_static("static", "doc")
+    copy_static("static", "docs")
     # generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursive("content", "template.html", "doc", basepath)
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
